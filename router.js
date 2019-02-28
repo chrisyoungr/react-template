f1b1047db95f76cc7cc913824c255ba21eda9d43
Manuel Hernandez <manuelhe46@gmail.com>
2019-02-28 09:07:54 -0800

    add routes

--- router.js
+++ router.js
@@ -3,3 +3,17 @@ const path = require('path');
 const express = require('express');
 
 const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  console.log('hit /');
+  res.sendFile(path.join(
+    __dirname, '..', '..', 'public', 'index.html'));
+});
+
+router.get('*', (req, res, next) => {
+  console.log('hit *');
+  res.sendFile(path.join(
+    __dirname, '..', '..', 'public', 'index.html'));
+});
+
+module.exports = router;
