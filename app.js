012c38d86593a27d03d2fc57c186b2de56da686c
Manuel Hernandez <manuelhe46@gmail.com>
2019-02-28 08:47:16 -0800

    add env and epxress variables

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+
+const env = process.env.NODE_ENV || 'development';
+const app = express();
