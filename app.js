a7ac3632577a6b686c3f759f53d559c1f32c4ee9
Manuel Hernandez <manuelhe46@gmail.com>
2019-02-28 08:50:54 -0800

    set up middleware and express

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ const bodyParser = require('body-parser');
 
 const env = process.env.NODE_ENV || 'development';
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(__dirname + '/public'));
