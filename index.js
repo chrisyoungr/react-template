2f96ad32c7daaa1a9cab686b834262591c6a650b
Manuel Hernandez <manuelhe46@gmail.com>
2019-02-28 07:55:56 -0800

    add app file and port definition to index

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 #!/usr/bin/env node
-var app = require('./app');
 var debug = require('debug')('react-template:server');
 var http = require('http');
+var app = require('./app');
+
 /**
  * Get port from environment and store in Express.
  */
 var port = normalizePort(process.env.PORT || '3013');
+
+/*
+ * Normalize a port into a number, string, or false.
+ */
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
 
 console.log("Works");
