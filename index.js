b29cd15a83fed167b1e9cb39540c66a20f92b075
Manuel Hernandez <manuelhe46@gmail.com>
2019-02-28 07:52:46 -0800

    add port definition to index

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
+var app = require('./app');
 var debug = require('debug')('react-template:server');
 var http = require('http');
+/**
+ * Get port from environment and store in Express.
+ */
+var port = normalizePort(process.env.PORT || '3013');
 
 console.log("Works");
