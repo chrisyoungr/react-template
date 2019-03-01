3ef6e49c9f2451fcb75e228d3f25131c85fec169
Chris Young <youngbankexperiments@gmail.com>
2019-03-01 10:49:32 -0500

    Added server folder

--- server/router.js
+++ server/router.js
@@ -7,13 +7,13 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   console.log('hit /');
   res.sendFile(path.join(
-    __dirname, 'public', 'index.html'));
+    __dirname,'../public/index.html'));
 });
 
 router.get('*', (req, res, next) => {
   console.log('hit *');
   res.sendFile(path.join(
-    __dirname, 'public', 'index.html'));
+    __dirname,'../public/index.html'));
 });
 
 module.exports = router;
