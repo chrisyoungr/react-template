9c351813c613024cafe0bb2cae0c06a04e4c96c4
Chris Young <youngbankexperiments@gmail.com>
2019-03-08 11:56:55 -0500

    revised package.json with eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,32 +4,32 @@ module.exports = {
     "plugins": ["react", "jsx-a11y", "import", "mocha"],
     
     "settings": {
-    "react": {
-    "createClass": "createReactClass", // Regex for Component Factory to use,
-    // default to "createReactClass"
-    "pragma": "React", // Pragma to use, default to "React"
-    "version": "15.0", // React version, default to the latest React stable release
-    "flowVersion": "0.53" // Flow version
-    },
-    "propWrapperFunctions": [ "forbidExtraProps" ], // The names of any functions used to wrap the
-    // propTypes object, e.g. forbidExtraProps.
-    // If this isn't set, any propTypes wrapped in
-    // a function will be skipped.
-    
-    },
-    "env": {
-    "es6": true,
-    "browser": true,
-    "node": true
-    },
-    "parserOptions": {
-        "ecmaVersion": 6,
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true,
-            "experimentalObjectRestSpread": true
-        }
-    },
+        "react": {
+            "createClass": "createReactClass", // Regex for Component Factory to use,
+            // default to "createReactClass"
+            "pragma": "React", // Pragma to use, default to "React"
+            "version": "15.0", // React version, default to the latest React stable release
+            "flowVersion": "0.53" // Flow version
+        },
+        "propWrapperFunctions": [ "forbidExtraProps" ], // The names of any functions used to wrap the
+        // propTypes object, e.g. forbidExtraProps.
+        // If this isn't set, any propTypes wrapped in
+        // a function will be skipped.
+        
+        },
+        "env": {
+            "es6": true,
+            "browser": true,
+            "node": true
+        },
+        "parserOptions": {
+            "ecmaVersion": 6,
+            "sourceType": "module",
+            "ecmaFeatures": {
+                "jsx": true,
+                "experimentalObjectRestSpread": true
+            }
+        },
         "rules": {
             "react/jsx-uses-react": "error",
             "react/jsx-uses-vars": "error",
