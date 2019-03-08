ce8785f82f95d7dccaf26a2ec4eacb39468504f4
Chris Young <youngbankexperiments@gmail.com>
2019-03-08 11:35:40 -0500

    Added app.js to render React into index.html

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Router} from 'react-router-dom';
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return(
+            <div>Hello World from React</div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('after build');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import App from './app';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+);
