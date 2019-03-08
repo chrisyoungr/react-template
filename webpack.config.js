bc5c05e297f349c36d9663fd33e95a092d42c57a
Chris Young <youngbankexperiments@gmail.com>
2019-03-08 11:04:58 -0500

    Updated webpack modules > rules

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,35 @@ module.exports = {
         path: path.resolve(__dirname,'public'),
         filename: 'bundle.js',
     },
+    modules: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader'
+            },
+            {
+                test: /.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react'],
+                    plugins:['@babel/plugin-proposal-class-properties']
+                },
+            },
+            {
+                test: /.(css)$/,
+                use: [{
+                    loader: "style-loader"
+                    }, {
+                        loader: "css-loader", options: {
+                        sourceMap: true
+                    }
+                }]
+            },
+        ]
+    },
     plugins: [
         new CopyWebpackPlugin ([
             { from: 'src/html/index.html', to: 'index.html' },
