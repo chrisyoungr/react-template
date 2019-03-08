9e314553f4f27b38672b46e006a43ca33906610a
Chris Young <youngbankexperiments@gmail.com>
2019-03-08 11:50:25 -0500

    changed modules to module in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         path: path.resolve(__dirname,'public'),
         filename: 'bundle.js',
     },
-    modules: {
+    module: {
         rules: [
             {
                 enforce: 'pre',
