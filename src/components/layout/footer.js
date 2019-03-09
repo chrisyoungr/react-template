cf8c865f4d044df6a8ffa7253b86eb03405707a0
Chris Young <youngbankexperiments@gmail.com>
2019-03-09 11:01:15 -0500

    Implemented bootstrap and react

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Navbar} from 'react-bootstrap';
 
 const Footer = ()=> (
     <div className="footer">Footer</div>
--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,30 +1,20 @@
 import React from 'react';
 import {Navbar,Nav,NavItem} from 'react-bootstrap';
+import history from '../history';
 
 export default class Header extends React.Component {
-    onHome() {
-        history.replace('/');
-        history.go(0);
-    }
-    onAbout() {
-        history.replace('/');
-        history.go(0);
-    }
     render() {
         return (
         <div className="header">
-            <Navbar id="mainNav" brand="Reporting Web App" fixedTop>
-                <Navbar.Header>
-                    <Navbar.Brand className="navbar-brand">
-                    <img src="/img/favicon.png" alt="logo" height="30" width="30" />&nbsp;
-                    </Navbar.Brand>
-                    Reporting Web App
-                    <Navbar.Toggle />
-                </Navbar.Header>
-                <Navbar.Collapse>
-                    <Nav pullRight>
-                        <NavItem eventKey={1} onClick={() => this.onHome()}>Home</NavItem>
-                        <NavItem eventKey={1} onClick={() => this.onAbout()}>About</NavItem>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="/">React-Bootstrap
+                <img src="/img/favicon.png" alt="logo" height="30" width="30" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/about">Link</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
