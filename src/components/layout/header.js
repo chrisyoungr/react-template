e8a9e241c2eb74172471a02229f9fa138d977cbd
Chris Young <youngbankexperiments@gmail.com>
2019-03-09 10:03:36 -0500

    added header.js navigation

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,8 +1,34 @@
 import React from 'react';
-import {Navbar} from 'react-bootstrap';
+import {Navbar,Nav,NavItem} from 'react-bootstrap';
 
-const Header = ()=> (
-    <div className="header">Header</div>
-);
-
-export default Header;
+export default class Header extends React.Component {
+    onHome() {
+        history.replace('/');
+        history.go(0);
+    }
+    onAbout() {
+        history.replace('/');
+        history.go(0);
+    }
+    render() {
+        return (
+        <div className="header">
+            <Navbar id="mainNav" brand="Reporting Web App" fixedTop>
+                <Navbar.Header>
+                    <Navbar.Brand className="navbar-brand">
+                    <img src="/img/favicon.png" alt="logo" height="30" width="30" />&nbsp;
+                    </Navbar.Brand>
+                    Reporting Web App
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav pullRight>
+                        <NavItem eventKey={1} onClick={() => this.onHome()}>Home</NavItem>
+                        <NavItem eventKey={1} onClick={() => this.onAbout()}>About</NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+        );
+    }
+}
